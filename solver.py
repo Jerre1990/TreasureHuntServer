1f87880218a31804deadf3dc79c242158e28d0ac
dietercaes <47667377+dietercaes@users.noreply.github.com>
2020-11-21 15:31:37 +0100

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,19 +1,19 @@
-import json
-from base64 import b64encode
+#import json
+#from base64 import b64encode
 from Crypto.Cipher import ChaCha20
-from Crypto.Random import get_random_bytes
-from base64 import b64decode
+#from Crypto.Random import get_random_bytes
+#from base64 import b64decode
 import requests
 
-url = 'http://192.168.1.21:8000'
+url = 'http://192.168.1.21:8000'    # Server location (change to your server)
 
+# Turning the hexadecimal values into bytes.
 message = bytes.fromhex("61009c2cdb0b1326bdccfc689a7fb6dec1a6479af2bd4833c4b9b99fa09f634da3")
 key = bytes.fromhex("56c358f80430fd6e3da571b06a4b301d26af980f08a509d288d13e70847ddcf4")
 nonce = bytes.fromhex("564401756dd4574e")
 
-ciphertext = message
 cipher = ChaCha20.new(key=key, nonce=nonce)
-plaintext = cipher.decrypt(ciphertext)
+plaintext = cipher.decrypt(message)
 
 r = requests.post(url + "/opdracht8", json={"bericht_ontcijferd": plaintext.decode('utf-8')})
 result = r.text
